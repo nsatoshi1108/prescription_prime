1fa72b1e3c0fe2b47e5514c5099cd8db53b562c3
yoshikiosuga <consultation.pharmacist@gmail.com>
2017-06-03 13:28:37 +0900

    Initial Commit

---
+++ prescription_prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prescription_prime
+//
+//  Created by nakagawa satoshi on 2017/06/03.
+//  Copyright © 2017年 yoshikiosuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
